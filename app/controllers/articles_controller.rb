863707dd1b7f6541258f10d8363854809693216a
Ilia.Koskin <kosua3@rambler.ru>
2014-11-17 18:59:07 +0400

    Entire Blog v1 commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,70 @@
+class ArticlesController < ApplicationController
+   
+    http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+    def show
+        @article = Article.find(params[:id]) 
+    end
+   
+
+    def index
+        @articles = Article.all
+    end
+
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path
+    end
+
+    private 
+        def article_params
+            params.require(:article).permit(:title, :text)
+        end
+
+
+end
+
+class CommentsController < ApplicationController
+    def create
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.create(comment_params)
+        redirect_to article_path(@article)
+    end
+
+    private
+        def comment_params
+            params.require(:comment).permit(:commenter, :body)
+        end
+end
